656459c202c1db31998c118f303311363126c09b
Parkersamad <parkersamad19@gmail.com>
2025-05-10 18:49:57 +0100

    Refactor registerUser function to include email validation and error handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,11 @@
-const registerUser = async (req, res) => {
-    res.send("Register User")
+const registerUser = (req, res) => {
+    if(!req.body.email){
+        res.status(400);
+         throw new Error("Please add an Email");
+   }
+    res.send("Register User");
 };
-
- 
 
 module.exports = {
     registerUser
-}
+};
