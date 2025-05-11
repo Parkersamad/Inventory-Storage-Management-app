99b442153eb3a74370cc59eb33a5cdaea0816131
Parkersamad <parkersamad19@gmail.com>
2025-05-11 15:01:42 +0100

    Implement password encryption in user model and remove redundant code in registerUser function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,5 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
-const bcrypt = require('bcryptjs');
 
 const registerUser = asyncHandler (async (req, res) => {
     const { name, email, password, phone} = req.body;
@@ -24,17 +23,12 @@ const registerUser = asyncHandler (async (req, res) => {
     throw new Error("User already exists");
    }
 
-   // Encrypt password before saving to DB
-
-   const salt = await bcrypt.genSalt(10);
-   const hashedPassword = await bcrypt.hash(password, salt);
-
 
    // Create new user
    const user = await User.create({
     name,
     email,
-    password: hashedPassword,
+    password,
     phone,
    });
 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema ({
 
@@ -21,13 +22,12 @@ password: {
     type: String,
     required : [true, "Please add a password"],
     minLength: [6, "Password must be at least 6 characters"],
-    // maxLength: [23, "Password must be at most 23 characters"],
+    maxLength: [23, "Password must be at most 23 characters"],
     trim: true,
 },
 phone: {
     type: String,
     required : [true, "Please add a phone number"],
-    unique: false,
     trim: true,
     match: [
         /^((\+91-?)|0)?[789]\d{9}$/,
@@ -43,7 +43,21 @@ photo: {
 
 },{
     timestamps: true,
-})
+});
+
+// Encrypt password before saving to DB
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+});
+
+
 
 const User = mongoose.model("User", userSchema)
 module.exports = User;
